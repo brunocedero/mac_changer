28800723beb4366c4492fa8d0fd9f0f37aa21dfe
Bruno <bruno.cedero@hotmail.com>
2022-01-06 16:38:25 -0300

    agregada funcionalidad de help

--- mac_changer.py
+++ mac_changer.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
 import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--interface", dest="interface", help="Interface para cambiar direccion MAC")
+parser.parse_args()
 
 interface = input("interface >")
 new_mac = input("nuevo MAC >")
 
-print("Cambiando la direccion MAC para" + interface + " a " + new_mac)
+print("[+] Cambiando la direccion MAC para" + interface + " a " + new_mac)
+
+
 
 subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
